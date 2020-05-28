8504724ba57d2d2f1e979a1a8061e1447db09789
Abaas Khorrami <abaas.khorrami@gmail.com>
2020-05-28 17:38:05 -0400

    static chart

--- src/scenes/Exchange/scenes/Options/components/Graph/index.js
+++ src/scenes/Exchange/scenes/Options/components/Graph/index.js
@@ -1,13 +1,96 @@
 import React from "react"
 import styled from "styled-components"
+import { Charts, ChartContainer, ChartRow, YAxis, LineChart, Legend, styler } from "react-timeseries-charts";
+import { TimeSeries } from "pondjs";
+
+import CrossHairs from "./components/CrossHairs"
 
 class Graph extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      x: null,
+      y: null
+    }
+  }
+
+  handleMouseMove(x, y) {
+    this.setState({ x, y })
+  }
+
+  handleMouseOut() {
+    console.log('mouse left')
+    this.setState({x: null, y: null})
+  }
   render() {
     const { focus } = this.props;
+
+    const history = [
+      [1400425947000, 7, 9, 8],
+      [1400425948000, 10, 20, 15],
+      [1400425949000, 12, 16, 14],
+      [1400425950000, 10, 14, 12]
+    ]
+
+    const data = {
+      name: "balance",
+      columns: ["time", "bid", "ask", "mid"],
+      points: history
+    };
+    
+    const series = new TimeSeries(data);
+    const upDownStyle = styler([
+      { key: "bid", color: "red" }, 
+      { key: "ask", color: "blue" },
+      { key: "mid", color: "green" }
+  ]);
     return (
       <Div>
         {!focus && `Select a contract`}
-        {focus && `Graph for ${this.props.focus.label}`}
+        {focus && 
+        <>
+          <ChartContainer
+            timeRange={series.range()}
+            width={300}
+            format={"%b-%d"}
+            // minDuration={1000 * 60 * 60 * 24}
+            onMouseMove={(x, y) => this.handleMouseMove(x, y)} 
+            onMouseOut={() => this.handleMouseOut()}
+          >
+            <ChartRow 
+              height="200">
+                <YAxis 
+                  id="axis1" 
+                  label="$"
+                  min={0}
+                  max={1.1*series.max("ask")}
+                  width="40"
+                  type="linear"
+                  format=",.1f"/>
+                <Charts>
+                    <LineChart
+                      axis="axis1"
+                      series={series}
+                      columns={["bid", "ask", "mid"]}
+                      interpolation={"curveLinear"}
+                      style={upDownStyle}
+                    />
+                    <CrossHairs x={this.state.x} y={this.state.y} />
+                </Charts>
+            </ChartRow>
+          </ChartContainer>
+          <Legend
+            type="swatch"
+            style={upDownStyle}
+            categories={[
+                { key: "bid", label: "BID" },
+                { key: "ask", label: "ASK" },
+                { key: "mid", label: "MID" }
+            ]}
+          />
+        </>
+        }
       </Div>
     )
   }
@@ -16,6 +99,7 @@ class Graph extends React.Component {
 const Div = styled.div`
 height: 100%; 
 border: 1px solid black;
+align-items: center;
 `;
 
 export default Graph;
